451b77343d6740ffa9e0272b4d48294069c1ff2f
m0rb <chris@cataclysmal.org>
2016-03-11 09:30:14 -0800

    Update simple_dns.sh
    [unixbeard stroking intensifies]

--- simple_dns.sh
+++ simple_dns.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
-if [ -z $1  ];then
+[ -z $1  ] && (
     echo "No arguments supplied"
     echo "Usage: simple_dns.sh [filename of domains, one per line] (optional filename to write results to)"
-    exit 1
-fi
+) && exit 1
 
-if [ -e $PWD/tmp_results_dns ]; then
-	rm -rf $PWD/tmp_results_dns
-fi
+MAX_PROC=5    # how many processes we can run at a time
+TMP=`mktemp`  # lets use builtin tempfile facilities
 
-for i in `cat $1`;do
-	nslookup $i | grep Address | tail -1 | cut -d":" -f2 >> $PWD/tmp_results_dns
-done
-if [ -z $2 ];then
-	echo "no filename specified for output. Putting into $PWD/dns_simple_results.txt"
-	cat tmp_results_dns | sort -u | sed 's#^\ ##g' >> $PWD/dns_simple_results.txt
-	rm -rf $PWD/tmp_results_dns
-else
-	cat tmp_results_dns | sort -u | sed 's#^\ ##g' >> $2.txt
-	rm -rf $PWD/tmp_results_dns
-fi
-exit 0
+(< $1 xargs -I{} -P${MAX_PROC} sh -c 'nslookup {} | awk '\''/Address:/{ayy=$2} END{print ayy}'\') >> ${TMP}
+
+[ -z $2 ] && (
+      echo "no filename specified for output. Putting into $PWD/dns_simple_results.txt"
+      sort -u ${TMP} | sed 's#^\ ##g' >> $PWD/dns_simple_results.txt
+      ) || (
+      sort -u ${TMP} | sed 's#^\ ##g' >> $2
+); rm ${TMP}
